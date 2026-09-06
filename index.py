c065b035f0328bab093a09382b0d038641acab97
anubhavrajput598 <anubhavrajput985@gmail.com>
2026-09-06 07:25:00 +0530

    index.py

--- index.py
+++ index.py
@@ -14,6 +14,7 @@ import re
 import os
 from datetime import datetime
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 # ─────────────────────────────────────────────
 #  CONFIG
